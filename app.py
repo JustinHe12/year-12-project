778cab188eb6c9e8272f91240567f5d1ec7418bb
Justin <23085@burnside.school.nz>
2026-05-11 20:26:45 +1200

    Finished the 3-column display for /questions page

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask, g, render_template, url_for
+from flask import Flask, g, render_template, url_for, redirect
 import sqlite3
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
 
 DATABASE = 'database.db'
 
 #Creates the app
 app = Flask(__name__)
-
+bycrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = 'TsISisdsihihsieref'
 
+
+login_manager = LoginManager() 
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 def get_db():
     db = getattr(g, '_database', None) #This line sets g._database to none if it dosen't already exist
@@ -32,14 +38,30 @@ def query_db(query, args=(), one=False):
     return (rv[0] if rv else None) if one else rv
 
 
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db = get_db()
+    cursor = db.execute("SELECT * FROM user WHERE id = ?", (int(user_id),))
+    row = cursor.fetchone()
+    
+    if row:
+        return User(id=row['id'], username=row['username'])
+    return None
+
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(),Length( #Input required means it must be filled out
-        min=4, max=20)], render_kw={"placeholder": "Username"})  #the other condition is that it must be between 4 to 20 letters
+    username = StringField(validators=[InputRequired()], render_kw={"placeholder": "Username"})  #the other condition is that it must be between 4 to 20 letters
     
-    password = PasswordField(validators=[InputRequired(),Length(
-        min=8, max=20)], render_kw={"placeholder": "Password"})
+    password = PasswordField(validators=[InputRequired()], render_kw={"placeholder": "Password"})
 
     submit = SubmitField("Register")
 
@@ -70,23 +92,68 @@ class LoginForm(FlaskForm):
 def home():
     return render_template("home.html")
 
+@app.route("/questions")
+#The page that displays all the questions after the user have logged in
+def questions():
+    db = get_db()
+    cursor = db.cursor()
+    sql = """
+    SELECT 
+    Questions.Question_ID, 
+    Questions.Question, 
+    Questions.Solution, 
+    WhereFrom.Name AS SourceName, 
+    Types.Name AS CategoryName
+    FROM Questions
+    JOIN WhereFrom ON Questions.Where_ID = WhereFrom.Where_ID
+    JOIN Types ON Questions.Type_ID = Types.Type_ID;
+    """
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template("questions.html", results=results)
 
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        db = get_db()
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM Users WHERE username = ?", (int(form.username.data, )))
+        row = cursor.fetchone
+        if row:
+            user = User(id = row['id'], username = row['username'], password = row['password'])
+        else:
+            user = None
+    
     return render_template('login.html', form = form)
 
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
     form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bycrypt.generate_password_hash(form.password.data) #this hashes the password
+
+        db = get_db() #connects to db 
+        cursor = db.cursor()
+
+        cursor.execute(
+            "INSERT INTO Users (username, password) VALUES (?, ?)", #inserts the username and hashed password into the database
+            (form.username.data, hashed_password)
+        )
+
+        db.commit() #saves the changes
+        return redirect(url_for('login')) #redirects them to the login form after the user have registered
+        
     return render_template('register.html', form = form)
 
 
 
-@app.route("/questions/<int:id>")
-def questions(id):
+@app.route("/question/<int:id>")
+def question(id):
     sql = """
     SELECT * FROM Questions
     JOIN WhereFrom ON WhereFrom.Where_ID = Questions.Where_ID
@@ -96,23 +163,14 @@ def questions(id):
     return render_template("question.html", question=results)
 
 
+@app.route('/dashboard', methods = ['GET','POST'])
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
 
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-"""   db = get_db()
-    cursor = db.cursor()
-    sql =  
-        SELECT Questions.Question_ID, WhereFrom.Name, Questions.Question, Types.name
-        FROM Questions
-        LEFT JOIN WhereFrom ON WhereFrom.Where_ID = Questions.Where_ID
-        LEFT JOIN Types ON Types.Type_ID = Questions.Type_ID;
-
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template("home.html", results=results)"""
-
 
